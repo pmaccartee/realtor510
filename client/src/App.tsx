d9b17b6a8cf1ecdd3fe27e19d4da92890d8d6938
Patrick MacCartee <patrick@realtor510.com>
2026-03-12 15:09:37 +0000

    Remove unused /julia React route; add /julia -> /julia-morgan redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import BlogDetail from "./pages/BlogDetail";
 import Reviews from "@/pages/Reviews";
 import Trends from "@/pages/Trends";
 import Waters from "@/pages/Waters";
-import Julia from "@/pages/Julia";
+
 import Neighborhoods from "@/pages/Neighborhoods";
 
 function Router() {
@@ -27,10 +27,8 @@ function Router() {
       <Route path="/reviews" component={Reviews}/>
       <Route path="/trends" component={Trends}/>
       <Route path="/waters" component={Waters}/>
-      <Route path="/julia" component={Julia}/>
       <Route path="/neighborhoods" component={Neighborhoods}/>
       <Route path="/trends/waters" component={Waters}/>
-      <Route path="/trends/julia" component={Julia}/>
       <Route component={NotFound} />
     </Switch>
   );
